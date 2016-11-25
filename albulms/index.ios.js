43da3e8384b3c5d0fd4c003cad5d366522c3f400
bekir <bekirtura1238@hotmail.com>
2016-11-25 21:31:04 +0300

    Albums Example Proje
    Proje

---
+++ albulms/index.ios.js
@@ -0,0 +1,19 @@
+// import a library to help create  a component
+
+import React from 'react';
+import {AppRegistry,View} from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+
+// Create a component
+const App = () =>(
+    <View style={{flex: 1}}>
+         <Header headerText={'Bekir albulms'}/> 
+         <AlbumList />
+    </View>
+   
+);
+// render it to device 
+AppRegistry.registerComponent('albulms',()=>App);
+
